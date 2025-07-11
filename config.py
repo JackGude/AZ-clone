f160bc5d8af1ccc77f75cc49387939143be463c0
Jack Gude <jgude2@unl.edu>
2025-07-11 12:52:38 -0500

    Updating config for v2

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # -----------------------------------------------------------------------------
 #  Project and File Paths
 # -----------------------------------------------------------------------------
-PROJECT_NAME = "alphazero-chess"
+PROJECT_NAME = "alphazero-chess-2"
 NUM_WORKERS = 4
 
 TENSOR_CACHE_DIR = "replay_cache"
@@ -33,7 +33,7 @@ AUTOMATE_DEFAULT_NUM_SELFPLAY_GAMES = 2000  # Number of self-play games to gener
 AUTOMATE_NUM_EVAL_GAMES = 48  # Number of games to play to compare models
 AUTOMATE_EVAL_TIME_LIMIT = 10  # Time in seconds per move for evaluation games
 AUTOMATE_WIN_THRESHOLD = 0.52  # Win rate needed for a candidate to be promoted
-AUTOMATE_WARMUP_GENS = 15  # Number of initial generations to run without evaluation
+AUTOMATE_WARMUP_GENS = 10  # Number of initial generations to run without evaluation
 
 # -----------------------------------------------------------------------------
 #  Self-Play Config (self_play.py)
@@ -41,7 +41,7 @@ AUTOMATE_WARMUP_GENS = 15  # Number of initial generations to run without evalua
 NUM_SELFPLAY_WORKERS = NUM_WORKERS  # Number of workers to use for self-play
 DEFAULT_NUM_SELFPLAY_GAMES = 4
 MAX_DRAW_GAMES = AUTOMATE_DEFAULT_NUM_SELFPLAY_GAMES  # Max draw games to store
-MAX_DECISIVE_GAMES = MAX_DRAW_GAMES * 3  # Max decisive games to store
+MAX_DECISIVE_GAMES = MAX_DRAW_GAMES * 4  # Max decisive games to store
 SPARRING_PARTNER_PROB = 0.33
 
 # MCTS Parameters
@@ -49,7 +49,7 @@ SELFPLAY_CPUCT = 1.41  # PUCT constant for MCTS exploration
 DIRICHLET_MODULO = 5  # Modulo for increasing Dirichlet noise
 DIRICHLET_EPSILON = 0.25  # Weight of Dirichlet noise
 DIRICHLET_ALPHA = 0.3  # Shape of Dirichlet noise
-TEMP_THRESHOLD = 40  # Number of moves to use temperature sampling
+TEMP_THRESHOLD = 30  # Number of moves to use temperature sampling
 
 # Game Termination Parameters
 SELFPLAY_MAX_MOVES = 200  # Cap for game length in self-play
@@ -68,7 +68,7 @@ WEIGHT_DECAY = 4.731e-5
 
 # Training Process Parameters
 TRAIN_WINDOW_SIZE = MAX_DECISIVE_GAMES + MAX_DRAW_GAMES  # Number of positions to sample for training
-BATCH_SIZE = 768
+BATCH_SIZE = 480
 MAX_EPOCHS = 20
 PATIENCE = 3  # Early stopping patience
 
@@ -83,10 +83,10 @@ DEFAULT_EVAL_CPUCT = 1.0  # PUCT constant for evaluation MCTS
 # Adjudication Parameters
 ADJUDICATION_START_MOVE = 60 # Don't check for draws before this move
 DRAW_ADJUDICATION_THRESHOLD = 0.10  # Adjudicate if abs(eval) is below this value
-DRAW_ADJUDICATION_PATIENCE = 20  # For this many consecutive moves
+DRAW_ADJUDICATION_PATIENCE = 16  # For this many consecutive moves
 
 # -----------------------------------------------------------------------------
 #  Stockfish Analysis Config (stockfish_analysis.py)
 # -----------------------------------------------------------------------------
-STOCKFISH_PATH = "models/stockfish/stockfish-16-x64-avx2.exe"
+STOCKFISH_PATH = "models/stockfish/stockfish-windows-x86-64-avx2.exe"
 NUM_STOCKFISH_WORKERS = NUM_WORKERS  # Number of workers to use for stockfish analysis
